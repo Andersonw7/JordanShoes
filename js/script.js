9e0e5673251dc41da2525d278bd0180414a3480b
Andersonw7 <126405995+Andersonw7@users.noreply.github.com>
2023-05-21 17:01:28 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle Dark Mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Load Light or Dark Mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+    if (darkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    // Save or remove Dark Mode
+    localStorage.removeItem("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+});
